c077888fc862afd2283ba62fb357d8eaf2a438af
xlybaby <31789146+xlybaby@users.noreply.github.com>
2018-09-08 17:10:15 +0800

    20180908

--- js/uc.js
+++ js/uc.js
@@ -9,7 +9,7 @@ var templateInstance = (function ($) {
 	var _BANNER = function(scenarioId){
 		
 	};
-	var _REFRESHBLOCK = function(scenarioId){
+	var _REFRESHBLOCK = function(scenarioId){  
 		
 	};
 	var _RANKLIST = function(scenarioId){
@@ -19,21 +19,212 @@ var templateInstance = (function ($) {
 		
 	};
 	var _COMPONENTSINFO = function(scenarioId){
+		var componentArray = [];
+		
 		var componentsRoot = $(".uc-edit-components[id='"+ scenarioId +"']");
 		if( componentsRoot.length>0 ) {
-			var containerSel = componentsRoot.children(".uc-edit-components-selector.container-selector");
-			if(containerSel.length>0){
-				for(var i=0;i<containerSel.length;i++){
-					var consel=containerSel[i];
+			var components = componentsRoot.children(".uc-edit-comp-r-editor-con");
+			if( components.length>0 ) {
+				for(var ci=0;ci<components.length;ci++ ) {
+					var compObj = {};
+					var comp = components[ci];
+					var containerSel = comp.children(".uc-edit-components-selector.container-selector");
+					var iteratorSel = comp.children(".uc-edit-components-selector.iterator-selector");
+					var elementSel = comp.children(".uc-edit-components-selector.element-selector");
+					var paginationSel = comp.children(".uc-edit-components-selector.pagination-selector");
+					compObj["containers"]={};
+					compObj["containers"]["selector"]=_collectSel(containerSel);
+					compObj["containers"]["iterators"]={};
+					compObj["containers"]["iterators"]["selector"]=_collectSel(iteratorSel);
 					
+					compObj["containers"]["iterators"]["items"]={};
+					compObj["containers"]["iterators"]["items"]["selector"]=_collectSel(elementSel);
+					if( elementSel.children("#in_hyperlink").hasClass("switch-on") ){
+						compObj["containers"]["iterators"]["items"]["link"] = 1;
+					} else
+						compObj["containers"]["iterators"]["items"]["link"] = 0;
+					
+					if( elementSel.children("#in_expand").hasClass("switch-on") ){
+						compObj["containers"]["iterators"]["items"]["extract"] = 1;
+					} else
+						compObj["containers"]["iterators"]["items"]["extract"] = 0;
+					
+					if( elementSel.children("#in_isimage").hasClass("switch-on") ){
+						compObj["containers"]["iterators"]["items"]["img"] = 1;
+					} else
+						compObj["containers"]["iterators"]["items"]["img"] = 0;
+					
+					compObj["pagination"]={};
+					compObj["pagination"]["selector"]=_collectSel(paginationSel);
+//					if(containerSel.length>0){
+//						for(var i=0;i<containerSel.length;i++){
+//							var consel=containerSel[i];
+//							
+//						}
+//					}
+					componentArray.push(compObj);
 				}
+				
 			}
 		}
+		return componentArray;
 	}
+	
+	var _collectSel = function( selector ) {
+		var nameInput = selector.children(".input_text_hint[name='txtName']");
+		var nameVal = null;
+		if( nameInput.length>0 )
+			nameVal = nameInput.val();
+		
+		var tagInput = selector.children(".input_text_hint[name='txtTag']");
+		var tagVal = null;
+		if( tagInput.length>0 )
+			tagVal = tagInput.val();
+		
+		var classInput = selector.children(".input_text_hint[name='txtClass']");
+		var classVal = null;
+		if( classInput.length>0 )
+			classVal = classInput.val();
+		
+		var idInput = selector.children(".input_text_hint[name='txtID']");
+		var idVal = null;
+		if( idInput.length>0 )
+			idVal = idInput.val();
+		
+		var xpathInput = selector.children(".input_text_hint[name='txtXPath']");
+		var xpathVal = null;
+		if( xpathInput.length>0 )
+			xpathVal = xpathInput.val();
+		
+		var indexInput = selector.children(".input_text_hint[name='txtIndex']");
+		var indexVal = null;
+		if( indexInput.length>0 )
+			indexVal = indexInput.val();
+		
+		var selector = {};
+		var attributes = {};
+		if(nameVal!=null)
+			selector["name"]=nameVal;
+		if(tagVal!=null)
+			selector["tag"]=tagVal;
+		if(classVal!=null)
+			selector["clazz"]=classVal;
+		if(idVal!=null)
+			selector["id"]=idVal;
+		if(xpathVal!=null)
+			selector["xpath"]=xpathVal;
+		if(indexVal!=null)
+			selector["index"]=indexVal;
+		
+		var attrs = selector.children(".custom-attrs");
+		if( attrs.length > 0 ) {
+			var inputs = attrs.children(".uc-edit-comp-r-editor-input-block");
+			for(var i=0; i<inputs.length; i++) {
+				var name=inputs[i].children("input[name='attrName']");
+				var val=inputs[i].children("input[name='attrValue']");
+				attributes[name.val()] = val.val();
+			}
+			if( inputs.length > 0 )
+				selector["attributes"] = attributes;
+		}
+		return selector;
+	}
+	
 	var _COMMONINFO = function(scenarioId){
 		var editPanel= $(".uc-edit-panel-main[id='"+ scenarioId +"']");
 		if( editPanel.length>0 ) {
+			var baseInfo = editPanel.children(".uc-edit-panel-block.basic-info");
+			var layoutInfo = editPanel.children(".uc-edit-panel-block.layout-info");
+			var sharingInfo = editPanel.children(".uc-edit-panel-block.sharing-info");
+			var scheduleInfo = editPanel.children(".uc-edit-panel-block.schedule-info");
+		
+			var baseInfoObj = {};
+			var layoutInfoObj = {};
+			var sharingInfoObj = {};
+			var scheduleInfoObj = {};
+			var sceneObj = {};
 			
+			if(baseInfo.children("#in_isauto").hasClass("switch-on"))
+				baseInfoObj["automation"]=1;//0:no auto,1:auto
+			else
+				baseInfoObj["automation"]=0;
+			
+			baseInfoObj["maxDuration"]=baseInfo.children("#in_maxDuration").val();
+			baseInfoObj["maxThreadNum"]=baseInfo.children("#in_maxThreadNum").val();
+			baseInfoObj["scenarioType"]=baseInfo.children("#in_scenarioType").val();
+			baseInfoObj["scenarioTypeName"]=baseInfo.children("#in_scenarioTypeName").val();
+			baseInfoObj["title"]=baseInfo.children("#in_title").val();
+			baseInfoObj["keywords"]=baseInfo.children("#in_keywords").val();
+			
+			baseInfoObj["scenelist"]=[];
+			
+			sceneObj["delay"]=baseInfo.children("#in_delay").val();
+			sceneObj["href"]=baseInfo.children("#in_href").val();
+			baseInfoObj["scenelist"].push(sceneObj);
+			
+			if( sharingInfo.children("#in_shareTheme").hasClass("switch-on") ) {
+				sharingInfoObj["shareTheme"] = 1;
+			} else
+				sharingInfoObj["shareTheme"] = 0;
+			if( sharingInfo.children("#in_shareWhole").hasClass("switch-on") ) {
+				sharingInfoObj["shareWhole"] = 1;
+			} else
+				sharingInfoObj["shareWhole"] = 0;
+			
+			layoutInfoObj["backgroundColor"] = layoutInfo.children("#in_bgcolor").val();
+			var borders = layoutInfo.children("#border-style");
+			if(borders.length>0){
+				var top = borders.children("#border-top");
+				var bottom = borders.children("#border-bottom");
+				var left = borders.children("#border-left");
+				var right = borders.children("#border-right");
+				
+				if(top.attr("clicked")==="true") {
+					layoutInfoObj["border-top"] = "1px solid #333333";
+				}
+				if(bottom.attr("clicked")==="true") {
+					layoutInfoObj["border-bottom"] = "1px solid #333333";
+				}
+				if(left.attr("clicked")==="true") {
+					layoutInfoObj["border-left"] = "1px solid #333333";
+				}
+				if(right.attr("clicked")==="true") {
+					layoutInfoObj["border-right"] = "1px solid #333333";
+				}
+				
+			}
+			
+			var shadow = layoutInfo.children("#shadow-style");
+			if(shadow.length>0){
+				if( borders.children("#style1").attr("clicked")==="true" ) {
+					layoutInfoObj["shadow"] = "0 0 3px #888888";
+					
+				}else if( borders.children("#style2").attr("clicked")==="true" ) {
+					layoutInfoObj["shadow"] = "2px 2px 3px #888888";
+					
+				}else if( borders.children("#style3").attr("clicked")==="true" ) {
+					layoutInfoObj["shadow"] = "-2px -2px 3px #888888";
+					
+				}else if( borders.children("#style4").attr("clicked")==="true" ) {
+					layoutInfoObj["shadow"] = "0 0 3px #888888 inset";
+					
+				}else if( borders.children("#style5").attr("clicked")==="true" ) {
+					layoutInfoObj["shadow"] = "2px 2px 3px #888888 inset";
+					
+				}else if( borders.children("#style6").attr("clicked")==="true" ) {
+					layoutInfoObj["shadow"] = "-2px -2px 3px #888888 inset";
+					
+				}
+			}
+			
+			scheduleInfoObj["interval"] = scheduleInfo.children("#in_schedule").val();
+			scheduleInfoObj["unit"]="SECONDS";
+			
+			baseInfoObj["sharing"]=sharingInfoObj;
+			baseInfoObj["layout"]=layoutInfoObj;
+			baseInfoObj["schedule"]=scheduleInfoObj;
+			
+			return baseInfoObj;
 		}
 	}
 	
